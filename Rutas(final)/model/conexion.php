0760e2e2821cbca2a9edf24b2921c5254e5ba336
zgarcia1112 <zgarcia1112@gmail.com>
2024-06-26 21:31:25 -0600

    Actualizar conexion.php

--- Rutas(final)/model/conexion.php
+++ Rutas(final)/model/conexion.php
@@ -1,15 +1,15 @@
-<?php
+p<?php
 /*
- * Proyecto    : Sistema de gestion de rutas de la red del transporte público
+ * Proyecto    : Sistema de gestion de rutas de la red del transporte pÃºblico
  * Descripcion : Aplicacion proveedora de servicios web para visualizacion de informacion de rutas.
  * Version     : 1.
  * Autor(es)   : Ana Karen Cordova Moran
- * Editado     : Emmanuel Garcia Cerriteño
+ * Editado     : Emmanuel Garcia CerriteÃ±o
  * Creacion    : 02-07-2015
  * Editado     : 10-08-2015
  * Decripcion
  * de clase    : Clase encargada de la conexion a la base de datos.
- * Copyright © 2015, Laboratorio de CPU
+ * Copyright Â© 2015, Laboratorio de CPU
  */
 
 class conexionBaseDatos
@@ -20,7 +20,7 @@ class conexionBaseDatos
 		 * Funcion encargada de crear la conexion a la base de datos.
 		 */
     session_start();
-    $conexion = pg_connect( "host=localhost port=5432 dbname=rutas user=postgres password=narcos" )
+    $conexion = pg_connect( "host=localhost port=5432 dbname=rutas user=postgres password=nulls" )
     or die('No se ha podido conectar al servidor.');
   }
 }
